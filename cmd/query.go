deee906120a603fb8756e2ce0b5671ddd631929a
Rodrigo de Avila <rdeavila@gmail.com>
2025-01-31 06:46:34 +0000

    fix: Remove unnecessary transaction query logic and replace with placeholder message

--- cmd/query.go
+++ cmd/query.go
@@ -3,9 +3,7 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // queryCmd represents the query command
@@ -17,40 +15,7 @@ This command lists transactions already executed by yum/dnf. By default, it
 displays transactions from the current host. However, when a query is passed,
 you can consult the transactions of other hosts.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Reading transactions...")
-
-		client := resty.New()
-		client.SetAuthToken(viper.GetString("postgrest.jwt_token"))
-
-		resp, err := client.R().EnableTrace().
-			Get(viper.GetString("postgrest.url") + "/transactions")
-
-		// Explore response object
-		fmt.Println("Response Info:")
-		fmt.Println("  Error      :", err)
-		fmt.Println("  Status Code:", resp.StatusCode())
-		fmt.Println("  Status     :", resp.Status())
-		fmt.Println("  Proto      :", resp.Proto())
-		fmt.Println("  Time       :", resp.Time())
-		fmt.Println("  Received At:", resp.ReceivedAt())
-		fmt.Println("  Body       :\n", resp)
-		fmt.Println()
-
-		// Explore trace info
-		fmt.Println("Request Trace Info:")
-		ti := resp.Request.TraceInfo()
-		fmt.Println("  DNSLookup     :", ti.DNSLookup)
-		fmt.Println("  ConnTime      :", ti.ConnTime)
-		fmt.Println("  TCPConnTime   :", ti.TCPConnTime)
-		fmt.Println("  TLSHandshake  :", ti.TLSHandshake)
-		fmt.Println("  ServerTime    :", ti.ServerTime)
-		fmt.Println("  ResponseTime  :", ti.ResponseTime)
-		fmt.Println("  TotalTime     :", ti.TotalTime)
-		fmt.Println("  IsConnReused  :", ti.IsConnReused)
-		fmt.Println("  IsConnWasIdle :", ti.IsConnWasIdle)
-		fmt.Println("  ConnIdleTime  :", ti.ConnIdleTime)
-		fmt.Println("  RequestAttempt:", ti.RequestAttempt)
-		fmt.Println("  RemoteAddr    :", ti.RemoteAddr.String())
+		fmt.Println("Command not implemented.")
 	},
 }
 
