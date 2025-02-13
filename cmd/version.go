1730208d2a29c55e4beddc49ab8e54bc28e6c805
Rodrigo de Avila <rdeavila@gmail.com>
2025-02-13 01:23:23 +0000

    feat: Enhance version command to display agent and server version numbers

--- cmd/version.go
+++ cmd/version.go
@@ -3,16 +3,37 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-const txlogVersion = "0.2"
+const agentVersion = "0.2"
+
+type ServerVersion struct {
+	Version string `json:"version"`
+}
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show version number",
+	Short: "Show agent and server version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(txlogVersion)
+		serverVersion := "unknown"
+		client := resty.New()
+		client.SetAllowGetMethodPayload(true)
+
+		var server ServerVersion
+		_, err := client.R().
+			SetHeader("Content-Type", "application/json").
+			SetResult(&server).
+			Get(viper.GetString("server.url") + "/v1/version")
+
+		if err == nil {
+			serverVersion = server.Version
+		}
+
+		fmt.Println("Agent version " + agentVersion)
+		fmt.Println("Server version " + serverVersion)
 	},
 }
 
