61032aa6d4ce8c5ce0358462d226fbc87512458f
Rodrigo de Avila <rdeavila@gmail.com>
2025-02-14 17:32:01 +0000

    feat: Disable default command completion for root command

--- cmd/root.go
+++ cmd/root.go
@@ -13,6 +13,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use: "txlog",
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
