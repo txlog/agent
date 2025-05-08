e2048fed0e7378a177c674d355baa69d24d72683
Rodrigo de Avila <rdeavila@gmail.com>
2025-05-07 21:31:47 -0300

    (feat) Add version check on txlog version

--- cmd/version.go
+++ cmd/version.go
@@ -18,30 +18,73 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show agent and server version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		serverVersion := "unknown"
-		client := resty.New()
-		client.SetAllowGetMethodPayload(true)
+		serverVersion := serverVersion()
+		latestAgentVersion := LatestAgentVersion()
 
-		var server ServerVersion
-		req := client.R().
-			SetHeader("Content-Type", "application/json").
-			SetResult(&server)
+		fmt.Println("Agent version: " + agentVersion)
+		fmt.Println("Server version: " + serverVersion)
 
-		if username := viper.GetString("server.username"); username != "" {
-			req.SetBasicAuth(username, viper.GetString("server.password"))
+		if latestAgentVersion != "" && latestAgentVersion != agentVersion {
+			fmt.Println("")
+			fmt.Println("────────────────────────────────────────────────────────")
+			fmt.Println("  A new Txlog Agent version is available: " + latestAgentVersion)
+			fmt.Println("  Go to https://txlog.rda.run/agent/latest for details")
+			fmt.Println("────────────────────────────────────────────────────────")
+			fmt.Println("")
 		}
-
-		_, err := req.Get(viper.GetString("server.url") + "/v1/version")
-
-		if err == nil {
-			serverVersion = server.Version
-		}
-
-		fmt.Println("Agent version " + agentVersion)
-		fmt.Println("Server version " + serverVersion)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+}
+
+// serverVersion retrieves the server version from the configured server URL.
+// It uses the resty library to make an HTTP GET request to the "/v1/version" endpoint.
+// If authentication is configured (username and password), it sets the Basic Auth header.
+// On success, it returns the server version string.
+// On failure (including network errors or invalid server response), it returns "unknown".
+func serverVersion() string {
+	client := resty.New()
+	client.SetAllowGetMethodPayload(true)
+
+	var server ServerVersion
+	req := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetResult(&server)
+
+	if username := viper.GetString("server.username"); username != "" {
+		req.SetBasicAuth(username, viper.GetString("server.password"))
+	}
+
+	_, err := req.Get(viper.GetString("server.url") + "/v1/version")
+
+	if err == nil {
+		return server.Version
+	}
+
+	return "unknown"
+}
+
+// LatestAgentVersion retrieves the latest agent version from a remote server.
+// It checks the 'agent.check_version' configuration to determine if version checking is enabled.
+// If enabled, it makes an HTTP GET request to "https://txlog.rda.run/docs/agent/version".
+// If the request is successful and returns a 200 status code, the function returns the body of the response as a string,
+// which represents the latest agent version.
+// If there is an error during the request or the status code is not 200, or if version checking is disabled,
+// the function returns an empty string.
+func LatestAgentVersion() string {
+	checkVersion := viper.Get("agent.check_version")
+
+	if checkVersion == nil || checkVersion == true {
+		response, err := resty.New().R().Get("https://txlog.rda.run/agent/version")
+
+		if err == nil {
+			if response.StatusCode() == 200 {
+				return string(response.Body())
+			}
+		}
+	}
+
+	return ""
 }
